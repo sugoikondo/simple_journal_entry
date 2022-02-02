2756eb558b0fe3a73a17c5cf54537f45a763de7c
kondo.atsumine <kondo.atsumine@moneyforward.co.jp>
2022-02-02 20:40:54 +0900

    fix misimports

--- api/src/main/kotlin/com/okeicalm/simpleJournalEntry/handler/mutation/users/CreateUserMutation.kt
+++ api/src/main/kotlin/com/okeicalm/simpleJournalEntry/handler/mutation/users/CreateUserMutation.kt
@@ -2,7 +2,7 @@ package com.okeicalm.simpleJournalEntry.handler.mutation.users
 
 import com.expediagroup.graphql.server.operations.Mutation
 import com.okeicalm.simpleJournalEntry.handler.type.UserType
-import com.okeicalm.simpleJournalEntry.usecase.user.CreateUserUseCaseImpl
+import com.okeicalm.simpleJournalEntry.usecase.user.CreateUserUseCase
 import com.okeicalm.simpleJournalEntry.usecase.user.UserCreateUseCaseInput
 import org.springframework.stereotype.Component
 
@@ -11,7 +11,7 @@ data class CreateUserInput(
 )
 
 @Component
-class CreateUserMutation(private val createUserUseCase: CreateUserUseCaseImpl): Mutation {
+class CreateUserMutation(private val createUserUseCase: CreateUserUseCase): Mutation {
     fun createUser(input: CreateUserInput): UserType {
         val output = createUserUseCase.create(UserCreateUseCaseInput(input.name))
         return UserType(output.user)
